3809312306a50304ef51d4ba315ed2660563f8ac
Roman-Hana <romanhanaua@gmail.com>
2021-06-04 16:03:03 +0300

    on pause v0.29

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fluid>
+      <v-app-bar color="orange" dark>
+        <v-spacer />
+        <v-btn outlined class="ml-3">
+          <router-link to="/">Main page</router-link>
+        </v-btn>
+        <v-btn outlined class="ml-3">
+          <router-link to="/photos">Photos page</router-link>
+        </v-btn>
+      </v-app-bar>
+    </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
